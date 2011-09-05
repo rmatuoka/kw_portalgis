f4db0557f3d29e3d1c8e174134521fcf298bc081
Rafael Matuoka <rmatuoka@korewa.com.br>
2011-09-05 17:20:51 -0300

    PUSH

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KwPortalgis::Application.config.secret_token = 'd4e5330353e6d1da62d8b63c6564c36058327429debc4a390d3d292c6e84cbf5493ab6fcbc4c729a0b21388648efb193da63bd41cb6580ef33ffaa89834cd550'
